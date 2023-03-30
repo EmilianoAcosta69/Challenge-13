432e1d72a2a41a8b0e9eec12c4156038a495fde7
Emiliano Acosta <emilianoacosta18@outlook.com>
2023-03-29 23:15:51 -0300

    challenge 13

---
+++ main.js
@@ -0,0 +1,42 @@
+// Punto 1 //
+
+
+let saludo1 = 'Hola ';
+let saludo2 = 'comision 48i';
+
+document.write (saludo1 + saludo2);
+
+// Punto 2 //
+
+let numero1 = 20;
+let numero2 = 5;
+
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let producto = numero1 * numero2;
+let divsion = numero1 / numero2;
+
+console.log ('Valores a sumar, restar, multiplicar y dividir: 20 y 5. ');
+console.log (" ");
+
+console.log ('Suma =' ,suma);
+console.log ('Resta =' ,resta);
+console.log ('Producto =' ,producto);
+console.log ('Division =' ,divsion);
+
+let par = numero1 % numero2;
+console.log (par==0 ? `El numero es par` : `El numero es impar`);
+console.log (" ");
+
+// Punto 3 //
+
+let numero3 = 48;
+
+let suma1 = suma + numero3;
+let resta1 = resta - numero3;
+let producto1 = producto * numero3;
+
+console.log ('Suma del 3er Numero =' ,suma1);
+console.log ('Resta del 3er Numero =' ,resta1);
+console.log ('Producto del 3er Numero =' ,producto1);
